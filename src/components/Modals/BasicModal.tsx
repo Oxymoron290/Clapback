b30a322ef3046ad08d3bfe9c45067fa80880878d
Timothy Sturm <timothysturm6@gmail.com>
2025-01-12 17:49:03 -0600

    Accepted comments on css classes

--- src/components/Modals/BasicModal.tsx
+++ src/components/Modals/BasicModal.tsx
@@ -6,13 +6,13 @@ const BasicModal: React.FC = () => {
   return (
     <>
       <div
-        className={`fixed top-0 left-0  w-full h-full overflow-x-hidden overflow-y-auto  ease-linear  z-sticky outline-0 ${
+        className={`fixed top-0 left-0  w-full h-full overflow-x-hidden overflow-y-auto  ease-linear  z-50 outline-0 ${
           openModal ? 'block bg-black bg-opacity-50 ' : 'hidden opacity-0'
         }`}
       >
         <div
           className={`relative w-auto m-2 transition-transform duration-300 ease-out pointer-events-none sm:m-7 sm:max-w-2xl sm:mx-auto lg:mt-20 ${
-            openModal ? 'transform-none' : '-translate-y-13'
+            openModal ? 'transform-none' : '-translate-y-12'
           }`}
         >
           <div className="relative flex flex-col w-full bg-white border border-solid pointer-events-auto dark:bg-gray-950 bg-clip-padding border-black/20 rounded-xl outline-0 min-h-10">
